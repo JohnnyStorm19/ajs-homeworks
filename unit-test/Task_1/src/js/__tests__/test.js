80e8ef3e0edae0c7e891ff155158fb548fbe149d
JohnnyStorm19 <112576084+JohnnyStorm19@users.noreply.github.com>
2023-04-24 13:11:38 +0300

    commit error directly

--- unit-test/Task_1/src/js/__tests__/test.js
+++ unit-test/Task_1/src/js/__tests__/test.js
@@ -8,7 +8,7 @@ test.each(
   [
     { name: 'Маг', health: 90, expected: 'healthy' },
     { name: 'Маг', health: 50, expected: 'wounded' },
-    { name: 'Маг', health: 10, expected: 'critical' },
+    { name: 'Маг', health: 10, expected: 'wounded' },
   ],
 )('testing character $name, with $health health', ({ name, health, expected }) => {
   const result = checkHealthBar({ name, health });
